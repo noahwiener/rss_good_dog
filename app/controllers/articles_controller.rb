1cf13d02e0d5c19cf53b78155131ed96bc1b8f67
noahwiener <noah.wiener@gmail.com>
2015-12-23 15:20:28 -0800

    create simple link-based RSS reader, saving each article to db for now

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,23 @@
 class ArticlesController < ApplicationController
 
   def index
+    @articles = Article.all
+    load_articles
+    render :index
+  end
+
+  private
+  def load_articles
+    require 'rss'
+    require 'uri'
     url = 'http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml'
     open(url) do |rss|
       feed = RSS::Parser.parse(rss)
-      @rss = feed.items
+      feed.items.each do |article|
+        @articles << Article.new({title: article.title, url: article.link})
+      end
     end
-    render :index
   end
+
+
 end
