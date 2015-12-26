aec60509eedf0c8fb582adcff99f3dd1f611e84a
noahwiener <noah.wiener@gmail.com>
2015-12-26 12:49:27 -0800

    add dog and styling

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :articles, only: [:create, :index, :destroy]
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
+
 end
