2710c3bec0fe401e6be3b89ab2088d55cb0fe1c3
noahwiener <noah.wiener@gmail.com>
2015-12-27 12:46:21 -0800

    new user redirected to article list, not profile. welcome text

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in(@user)
-      render :show
+      redirect_to articles_url
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
